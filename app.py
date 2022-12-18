5795fbf8f15ad0f74547301d3271bfb3eda06ad0
Eli Stouraitis <h.stoyraitis@gmail.com>
2022-12-18 16:48:52 +0200

    IMPROVE categorization logic

--- app.py
+++ app.py
@@ -10,40 +10,122 @@ from fuzzywuzzy import fuzz
 class ProductTitle:
 
     topics: dict[str: re.Pattern] = {
-    "color": ['-?ΛΕΥΚ\w-?', '-?ΑΣΠΡ\w-?', '-?ΓΚΡΙ-?', 
-            '-?ΕΚΡΟΥ-?', '-?ΜΠΕΖ-?', '-?ΜΑΥΡ\w-?', 
-            "-?ΑΝΘΡΑΚΙ-?", '-?ΚΟΚΚΙΝ\w-?', "-?ΜΠΟΡΝΤΩ-?", 
-            "-?ΡΟΖ-?", "-?ΜΩΒ-?", '-?ΚΙΤΡΙΝ\w\w?-?', 
-            '-?ΠΡΑΣΙΝ\w\w?-?', "-?ΦΥΣΤΙΚΙ-?", "-?ΒΕΡΑΜΑΝ-?", 
-            '-?ΜΠΛΕ-?', '-?ΣΙΕΛ-?', 'ΓΑΛΑΖΙ\w', 
-            '-?ΑΣΗΜΙ-?', '-?ΧΡΥΣ\w\w?-?', '-?ΜΟΥΣΤΑΡΔΙ-?',
-            "-?ΠΕΤΡΟΛ-?","Σ?Κ?Ο?Υ?Ρ?Ο?\s?-?ΚΑΦΕ-?", "-?ΛΑΔΙ-?",
-            "-?ΣΑΜΠΑΝΙ-?"],
-    "brand": ["INART", "ESPIEL", "KENTIA", "ZAROS", "AI DECORATION", "CLICK", "GUY_LAROCHE", "SAINTCLAIR", "SB_HOME", "SBABY", "BLE"],
-    "grouping": ["ΣΕΤ \d\d?\s?\S*", "ΣΕΤ \d\d?\s?\S*",  "ΣΕΤ ΤΩΝ \d\d?", "ΣΕΤ\d\d?", "SET", "^ΣΕΤ\s", "ΣΕΤ\s", "\d\d?\sΤΕΜ\S*", '\s(TEM)\s', "\s(ΤΕΜ)\s"],
-    "dimension": ["[ΦΔDF]?\d{1,2},?\d?X\d{1,2},?\d?", "[ΦΔDF]?\d{1,2},?\d?Χ\d{1,2},?\d?", "[^0+](\d{1,2},?\d?)\s"],
-    "unit": ["CM\w*\W?", "ΕΚ\w*\W?", "ΜΕΤΡ\w?\W?", "ML"],
-    "sku": ["ΚΩΔ:\S+\d", "\S+\d"],
-    "material": ['ΞΥΛ?Ι?Ν?\w?\w?', 'ΜΕΤΑΛΛ?Ι?Κ?\w?\w?', "ΨΑΘΙΝ\w\w?", "POLYRESIN", "ΠΟΛΥΕΣΤΕΡ\w?", "POLYESTER", "ΠΟΛΥΡΕΖΙΝ", "ΡΗΤΙΝΗ\w", "ΓΥΑΛ[^Α]\w*", "ΚΕΡΑΜΙΚ\w\w?", "ΥΦΑΣΜ?Α?Τ?Ι?Ν?\w\w?", "ΒΕΛΟΥΔΙ?Ν?\w?\w?", "FIBERGLASS"],
+    "color": ['-?ΛΕΥΚ\w[Σ]?-?', '-?ΑΣΠΡ\w[Σ]?-?', '-?ΓΚΡΙ-?', 
+            '-?ΕΚΡΟΥ-?', '-?ΜΠΕΖ-?', '-?ΜΑΥΡ\w[Σ]?-?', 
+            "-?ΑΝΘΡΑΚΙ-?", '-?ΚΟΚΚΙΝ\w\w?-?', "-?ΜΠΟΡΝΤΩ-?", 
+            "-?ΡΟΖ-?", "-?ΜΩΒ-?", '-?ΚΙΤΡΙΝ\w[Σ]?-?', 
+            '-?ΠΡΑΣΙΝ\w[Σ]?-?', "-?ΦΥΣΤΙΚΙ-?", "-?ΒΕΡΑΜΑΝ-?", 
+            '-?ΜΠΛΕ-?', '-?ΣΙΕΛ-?', 'ΓΑΛΑΖΙ\w[Σ]?', 
+            '-?ΑΣΗΜΙ-?', '-?ΧΡΥΣ\w[Σ]?-?', '-?ΜΟΥΣΤΑΡΔΙ-?',
+            "-?ΠΕΤΡΟΛ-?","-?ΣΚΟΥΡΟ-?\s?", "-?ΚΑΦΕ-?", "-?[^K]?ΛΑΔΙ-?",
+            "-?ΣΑΜΠΑΝΙ-?", "-?NATURAL-?", "-?ΔΙΑΦΑΝ\w[Σ]?-?", 
+            "[\\\s-]?ΚΡΕΜ[\\\s-]?", "-?Τ\wΡΚΟΥΑΖ-?", "ΜΠΡΟΝΖΕ"],
+    "brand": ["INART", "ESPIEL", "KENTIA", "ZAROS", "AI DECORATION", "CLICK", "GUY LAROCHE", "SAINT CLAIR", "SAINTCLAIR", "SB HOME", "SBABY", "BLE"],
+    "grouping": ["ΣΕΤ \d\d?\s?\S*", "ΣΕΤ \d\d?\s?\S*",  "ΣΕΤ ΤΩΝ \d\d?", "ΣΕΤ\d\d?", "SET", "^ΣΕΤ\s", "ΣΕΤ\s", "\d\d?\sΤΕΜ\S*", '\s(TEM)\s', "\s(ΤΕΜ)\s", "\sS\s\d\d?", "^S\s\d\d?"],
+    "dimension": ["[ΦΔDF]\d\S+\s?\d?\d?\d?\s?[CM,ML,L,ΕΚ]*", "\S*\d[XΧ]\d\S*\s?\d?\d?\d?\s?[CM,ML,L,ΕΚ]*", "\S*\d\s?\d?\d?\d?\s?[CMLΕΚΧΙΛ]+\s"],
+    # "unit": ["CM\w*\W?", "ΕΚ\w*\W?", "ΜΕΤΡ\w?\W?", "ML"],
+    # "sku": ["ΚΩΔ:\S+\d", "\S+\d"],
+    "material": ["ΑΛΟΥΜΙΝ\S*", "COTTON", "ΠΟΡΣΕΛΑΝ\S*", "ΜΠΑΜΠΟΥ", "BAMBOO", 'ΞΥΛ\S*', 'ΜΕΤΑΛΛ?Ι?Κ?\w?\w?', "ΨΑΘΙΝ\w\w?", "POLYRESIN", "ΠΟΛΥΕΣΤΕΡ\w?", "POLYESTER", "ΠΟΛΥΡΕΖ\S*", "ΡΗΤΙΝΗ\w", "ΓΥΑΛ[^Α]\S*", "ΚΕΡΑΜΙΚ\w\w?", "ΥΦΑΣΜ?Α?Τ?Ι?Ν?\w\w?", "ΒΕΛΟΥΔΙ?Ν?\w?\w?", "FIBERGLASS"],
     }
 
     def __init__(self, title, debug = False):
         self.debug = debug
-        self.original_title: str = title
-        self.normalized_title: str = self.normalize()
-        self.normalized_words = self.normalized_title.split(" ")
-        self.matched_words = self.classifier()
-        self.verbose_title = self.add_descriptors()
-        self.product = self.extract_product()
-        self.brand = self.extract("brand")
-        self.code = self.extract("code")
-        self.unit = self.extract("unit")
-        self.grouping = self.extract("grouping")
-        self.dimension = self.extract("dimension")
-        self.color = self.extract("color")
-        self.material = self.extract("material")
-        self.entropy_title = self.calculate_entropy(self.normalized_title)
-        self.entropy_product = self.calculate_entropy(self.product)
+        self.original_title: str = title.strip()
+        self.title = self.original_title
+        self.normalized_title, self.brand, self.code = self.simplify_title()
+        self.grouping = self.classifier("grouping")
+        self.color = self.classifier("color")
+        self.material = self.classifier("material")
+        self.dimension = self.classifier("dimension")
+        self.product = self.extract_product().title()
+
+
+
+
+
+        # self.normalized_words = self.normalized_title.split(" ")
+        # self.matched_words = self.classifier()
+        # self.verbose_title = self.add_descriptors()
+        # self.product = self.extract_product()
+        # # self.brand = self.extract("brand")
+        # # self.code = self.extract("code")
+        # self.unit = self.extract("unit")
+        # self.grouping = self.extract("grouping")
+        # self.dimension = self.extract("dimension")
+        # self.color = self.extract("color")
+        # self.material = self.extract("material")
+        # self.entropy_title = self.calculate_entropy(self.normalized_title)
+        # self.entropy_product = self.calculate_entropy(self.product)
+
+
+    def simplify_title(self):
+        title = self.normalize()
+        brand_ = ""
+        possible_SKU = ""
+        for brand in self.topics['brand']:
+            if fuzz.partial_ratio(title, brand) == 100:
+                brand_ = brand
+                title = title.replace(brand, "").strip()
+                break
+        possible_SKU = title.split(" ")[-1]
+        if re.search("\d", possible_SKU):
+            title = title.replace(possible_SKU, "")
+        if self.debug:
+            print(possible_SKU)
+            print(brand_)
+            print(title)
+            print()
+        # self.brand = brand_
+        # self.code = possible_SKU
+        return title, brand_, possible_SKU.replace("ΚΩΔ:", "")
+
+
+    def normalize(self) -> str:
+        d = {ord('\N{COMBINING ACUTE ACCENT}'):None}                                            # unicodedata library
+        normalized_title = ud.normalize("NFD", self.original_title).upper().translate(d)        # code to remove diacritics
+        normalized_title = (
+            normalized_title
+            .replace(" ", " ")
+            .replace(" ", " ")
+            .replace("/", " ")
+            # .replace("-", " ")
+            .replace("\"", "")
+            .replace("\'", "")
+            .replace(", ", " ")
+            .replace("(", " ")
+            .replace(")", " ")
+            .replace(".", "")
+            .replace(",1", ".1")
+            .replace(",2", ".2")
+            .replace(",3", ".3")
+            .replace(",4", ".4")
+            .replace(",5", ".5")
+            .replace(",6", ".6")
+            .replace(",7", ".7")
+            .replace(",8", ".8")
+            .replace(",9", ".9")
+            .replace(",", " ")
+            .replace(".1", ",1")
+            .replace(".2", ",2")
+            .replace(".3", ",3")
+            .replace(".4", ",4")
+            .replace(".5", ",5")
+            .replace(".6", ",6")
+            .replace(".7", ",7")
+            .replace(".8", ",8")
+            .replace(".9", ",9")
+            # .replace(" ΤΕΜ", "ΤΕΜ")
+            # .replace("+ ", "+")
+            .replace("ΚΩΔ ", "ΚΩΔ:")
+            .replace("ΣΕΤ ΤΩΝ", "ΣΕΤ")
+            .replace("ΧΡΩΜΑ", "")
+            # .replace("AI DECORATION", "AI_DECORATION")
+            # .replace("SB HOME", "SB_HOME")
+            # .replace("GUY LAROCHE", "GUY_LAROCHE")
+        )
+        normalized_title = re.sub("\s{2,}", " ", normalized_title)
+        normalized_title = re.sub("\d*%", "", normalized_title)
+        return normalized_title
 
 
     def get_info(self) -> list:
@@ -93,65 +175,20 @@ class ProductTitle:
         return matched_words
 
 
-    def classifier(self) -> bool:
+    def classifier(self, topic) -> bool:
         matched = {}
-        for topic, patterns in self.topics.items():
-            for pattern in patterns:
-                search = re.findall(pattern, self.normalized_title)
-                if search:
-                    for item in search:
-                        matched[item] = topic
+        patterns = self.topics[topic]
+        for pattern in patterns:
+            search = re.findall(pattern, self.normalized_title)
+            if search:
+                for item in search:
+                    item = item.strip()
+                    matched[item] = topic
         if self.debug:
             print(self.original_title)
             print(matched)
-        return matched
+        return list(set(list(matched.keys())))
 
-
-
-    def normalize(self) -> str:
-        d = {ord('\N{COMBINING ACUTE ACCENT}'):None}                                            # unicodedata library
-        normalized_title = ud.normalize("NFD", self.original_title).upper().translate(d)        # code to remove diacritics
-        normalized_title = (
-            normalized_title
-            .replace(" ", " ")
-            .replace(" ", " ")
-            .replace("/", " ")
-            # .replace("-", " ")
-            .replace("\"", "")
-            .replace("\'", "")
-            .replace(", ", " ")
-            .replace("(", " ")
-            .replace(")", " ")
-            .replace(".", "")
-            .replace(",1", ".1")
-            .replace(",2", ".2")
-            .replace(",3", ".3")
-            .replace(",4", ".4")
-            .replace(",5", ".5")
-            .replace(",6", ".6")
-            .replace(",7", ".7")
-            .replace(",8", ".8")
-            .replace(",9", ".9")
-            .replace(",", " ")
-            .replace(".1", ",1")
-            .replace(".2", ",2")
-            .replace(".3", ",3")
-            .replace(".4", ",4")
-            .replace(".5", ",5")
-            .replace(".6", ",6")
-            .replace(".7", ",7")
-            .replace(".8", ",8")
-            .replace(".9", ",9")
-            # .replace(" ΤΕΜ", "ΤΕΜ")
-            .replace("+ ", "+")
-            .replace("ΚΩΔ ", "ΚΩΔ:")
-            .replace("ΣΕΤ ΤΩΝ", "ΣΕΤ")
-            .replace("AI DECORATION", "AI_DECORATION")
-            .replace("SB HOME", "SB_HOME")
-            .replace("GUY LAROCHE", "GUY_LAROCHE")
-        )
-        normalized_title = re.sub("\s{2,}", " ", normalized_title)
-        return normalized_title
 
     def add_descriptors(self):
         title = self.normalized_title
@@ -161,9 +198,20 @@ class ProductTitle:
 
 
     def extract_product(self) -> str:
-        product = self.verbose_title
-        product = re.sub("\$\$\$\S+\)\s?", "", product)
-        return product.replace("  ", " ").replace("  ", " ")
+        product = self.normalized_title+" "
+        items = [self.dimension, self.grouping, self.material, self.color]
+        string = ""
+        for item in items:
+            word = " ".join(item)
+            string += f" {word}"
+        string = re.sub("\s{1,}", " ", string)
+        words = string.split(" ")
+        print(words)
+        for word in words:
+            if word:
+                # word = word.replace("&&&", " ")
+                product = product.replace(f" {word} ", "  ")
+        return re.sub("\s{1,}", " ", product).strip()
 
 
     def extract(self, kw) -> list[str]:
@@ -180,27 +228,38 @@ class ProductTitle:
         return score
         
 
-        
 
+if "dumped_product_titles.txt" in os.listdir():
+    print("Loading Cache - To load from scratch, delete 'dumped_product_titles.txt'")
+    with open("dumped_product_titles.txt", 'r', encoding='utf-8') as f:
+        titles = f.readlines()
+else:
+    filename = "products_20221215-044713.xlsx"
+    print("Loading Workbook")
+    wb = opxl.load_workbook(filename)
+    sheet = wb["Products"]
+
+    titles = [x[0].value for x in sheet["f2:f100000"] if x[0].value]
+    with open("dumped_product_titles.txt", 'w', encoding='utf-8') as f:
+        f.write("\n".join(titles))
+        wb.close()
+
+
+def test():
+    pt = ProductTitle
+    e = {}
+    for i in range(50):
+        no = randint(0,60000)
+        # print(no)
+        e[i] = pt(titles[no])
+        print(f"{i} - {e[i].title}")
+        print(e[i].brand, e[i].code, e[i].grouping, e[i].color, e[i].material, e[i].dimension, sep="\n")
+        print(e[i].product)
+        print()
 
 
 def main():
 
-    if "dumped_product_titles.txt" in os.listdir():
-        print("Loading Cache - To load from scratch, delete 'dumped_product_titles.txt'")
-        with open("dumped_product_titles.txt", 'r', encoding='utf-8') as f:
-            titles = f.readlines()
-
-    else:
-        filename = "products_20221215-044713.xlsx"
-        print("Loading Workbook")
-        wb = opxl.load_workbook(filename)
-        sheet = wb["Products"]
-
-        titles = [x[0].value for x in sheet["f2:f100000"] if x[0].value]
-        with open("dumped_product_titles.txt", 'w', encoding='utf-8') as f:
-            f.write("\n".join(titles))
-        wb.close()
 
     data = []
     for i, title in enumerate(titles):
