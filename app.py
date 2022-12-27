fb7e862a22bd5851798dd74220caf1aee474b785
Eli Stouraitis <h.stoyraitis@gmail.com>
2022-12-27 21:27:46 +0200

    IMPROVE find function

--- app.py
+++ app.py
@@ -9,10 +9,21 @@ from fuzzywuzzy import fuzz
 from collections import Counter
 import win32ui
 from pprint import pprint
+from time import perf_counter
 
 from product_class import Product
 
 class ProductTitle(Product):
+
+    regex_patterns = {
+        'parenth': re.compile("\((.+?)\)"),
+        'brand': '', #uses fuzzy search
+        'series': '', #simple conditionals
+        'grouping': [re.compile("([Ss])(/\d*)\s"), re.compile("(\s\d\d?)?(\s[SΣsσ][ΕεEe][TtΤτ]\s\d{1,4}\s\S+)\s")], #
+        'SKU': '', #conditionals
+        'volume': re.compile("\| \d+,?\d?\s?[MmLlCc][TtLlCc\s]"),
+        'dimension': re.compile("\| [ΦΔDF]?\s?\d[0-9X\-,\.]+\s?[EeΕεCc^M^m^Μ^μ^T^t^Τ^τ]?[ΚκKkMm^C^c]?\.?\s?"),
+    }
 
 
     def __init__(self, title, debug = False):
@@ -63,138 +74,124 @@ class ProductTitle(Product):
                     os.startfile(filename)
 
 
-    #find() function is huge and needs to be simplified with more descriptive names and standarized output. For now I added comments to navigate it
-    #but it does way too many string manipulations without any standard, which leads to bugs that take time to resolve.
-    #Thankfully the recursion doesn't really need to return any information. It just passes the modified input around. Perhaps I should
-    #get rid of it but it gets points for style. Sorry.
+    #find() function is huge and needs to be simplified with more descriptive names and standarized output. Once everything
+    #works as it should be, I will split it into multiple functions 
 
-    def find(self, string: str, og_title = "", parenth = "", brand = "", grouping = "", code = "", series = "", first: int = 1) -> dict[str:str]:
+    def find(self, string: str) -> dict[str:str]:
 
-        if first:                                                                       #Necessary flag due to recursion.
-            og_title = string                                                           #Every bit of code here only needs to run once.
-            # print('\n'+string.strip())
-            string = string + " |"
-            string = string.upper()
+                                                          
+        og_title = string                                                           
+        # print('\n'+string.strip())
+        string = string + " |"
+        string = string.upper()
 
 
-            #remove info in parenthesis
-            parenth = re.findall("\((.+?)\)", string)                                   #the idea is that info in parenthesis
-            if parenth:                                                                 #is just miscellaneous info
-                for item in parenth:                                                    #and doesn't need to be looked upon too analytically
-                    string = string.replace(f" ({item})", "")
-                parenth = " ".join(parenth)
-            else:
-                parenth = ""
+        #remove info in parenthesis !!
+        parenth = re.findall(self.regex_patterns['parenth'], string)                                   #the idea is that info in parenthesis
+        if parenth:                                                                 #is just miscellaneous info
+            for item in parenth:                                                    #and doesn't need to be looked upon too analytically
+                string = string.replace(f" ({item})", "")
+                string = re.sub("\s{2,}", " ", string)
+            parenth = " ".join(parenth)
+        else:
+            parenth = ""
 
-            #debrand
-            for brand_name in ProductTitle.topics['brand']:
-                if fuzz.partial_ratio(string, brand_name.upper()) == 100:       #Brand names generally are very easy to isolate and could
-                    brand = brand_name                                                  #be handled with a simple search, but to avoid possible
-                    string = string.replace(brand.upper(), "").strip()          #typos, I did a fuzz search instead
-                    break
+        #debrand !!
+        brand = ""
+        for brand_name in ProductTitle.topics['brand']:
+            if fuzz.partial_ratio(string, brand_name.upper()) == 100:               #Brand names generally are very easy to isolate and could
+                brand = brand_name                                                  #be handled with a simple search, but to avoid possible
+                string = string.replace(brand.upper(), "").strip()                  #typos, I did a fuzz search instead
+                break
 
 
+        #deseries !!
+        series = ""                                                                 #Some products are part of named series that usually
+        words = string.split()[0:2]                                                 #come before the product. While it's not easy to isolate
+        if re.search("\d+", words[1]) and re.search("[a-zA-Z]+", words[0]):         #them, if they follow a "Name ##" pattern, then they are 
+            series = " ".join(words)                                                #almost certainly a match.
+            string = string.replace(series + " ", "") 
 
-            #deseries
-            series = ""                                                                 #Some products are part of named series that usually
-            words = string.split()[0:2]                                                 #come before the product. While it's not easy to isolate
-            if re.search("\d+", words[1]) and re.search("[a-zA-Z]+", words[0]):         #them, if they follow a "Name ##" pattern, then they are 
-                series = " ".join(words)                                                #almost certainly a match.
-                string = string.replace(series + " ", "") 
 
-            #degroup
-            patterns = ["()([Ss]/\d*)\s", "(\s\d\d?)?(\s[SΣsσ][ΕεEe][TtΤτ]\s\d{1,4}\s\S+)\s"]          #Poonto adds the tag 'ΣΕΤ # τεμ.' to certain products
-            for pattern in patterns:                                                    #and unless there is a typo, this should always work fine.
-                grouping = re.findall(pattern, string)
-                if grouping:
-                    grouping = " ".join(list(grouping[0])).strip()
-                    string = string.replace(grouping+" ", "").strip()
-                    break
-                grouping = ""
-            
-            first = 0
-
-        head, *tail = string.split("|")
-
-        # recursive call to split string into parts with alphanumerics
-        pattern = "(.*)\s(\w*\d.*)\s?"                                                  #recursively marks alphanumeric words and numbers
-        results = re.findall(pattern, head)                                             #from right to left.
-        if results:
-            results = list(results[0])
-            head = " | ".join(results)
-            return self.find(head + "|" + "|".join(tail), og_title, parenth, brand, grouping, code, series, 0)
-        string = string.strip("|")
+        #degroup !!
+        #patterns = ["([Ss])(/\d*)\s", "(\s\d\d?)?(\s[SΣsσ][ΕεEe][TtΤτ]\s\d{1,4}\s\S+)\s"]#, "([SΣsσ][ΕεEe][TtΤτ]).*?(\d\d?\s[TtΤτ][ΕεEe][MmΜμ]\.?)"]          #Poonto adds the tag 'ΣΕΤ # τεμ.' to certain products
+        for pattern in self.regex_patterns['grouping']:                                                    #and unless there is a typo, this should always work fine.
+            grouping = re.findall(pattern, string)
+            if grouping:
+                grouping = grouping[0] if type(grouping) != type("string") else grouping 
+                for word in grouping:
+                    if word and word != " ":
+                        string = string.replace(word, "").strip()
+                grouping = " ".join(list(grouping[0])).strip()
+                break
+            grouping = ""
         
+        string = re.sub("\s{2,}", " ", string)
+
+        
+        ####### Alphanumeric split with pipes
+        pattern = "(\S*\d\S*)\s?"
+        string = re.sub(pattern, "| \\1 ", string).strip("|\n")
+
 
         #de-SKU
-        check_string = string.split("|")[-1]                                            
+        code = ""
+        check_string = string.split("|")[-1].strip()                                         
         if len(check_string) > 3:                                                       #SKUs have more than 3 characters
             found = re.search("\d[XxΧχ]\d", check_string)                               #if they match too closely to a dimension pattern
             if not found:                                                               #then the program should ignore them
                 found = re.search("\S+\d\S+", check_string)
                 if found:
                     if re.search("[a-zA-Z]", check_string):                             #if the SKU contains letters, it should never begin
-                        if not re.search("(\s[a-zA-Z])", check_string):                 #with a numeric character.
+                        if not re.search("^[a-zA-Z]", check_string):                 #with a numeric character.
                             found = ""                                                  
                 if found:# and found.span()[1]-found.span()[0] > 2:
-                    string = string.replace(check_string, "")
+                    string = string.replace(check_string, "").strip().strip("|").strip()
                     string = string.replace("κωδ.", "").replace("ΚΩΔ.", "")
-                    code = check_string.strip().strip("|").strip()
+                    code = check_string
 
 
-        #find volumes
-        volumes = ""                                                                    #while dimensions don't necessarily show the unit
-        for substring in string.split("|"):                                             #volume always does, otherwise it's impossible to 
-            result = re.findall("\s(\d+[,\.]?\d*\s?[MmLl]{1}[TtLl]?)\s", substring)             #communicate the intent to a buyer, unless there is
-            if type(result) == type('a'):                                               #a typo, which is beyond the scope of this program
-                result = [result]
-            volumes += " ".join(result)
-        if volumes:
-            for volume in volumes.split():
-                string = string.replace(f"{volume} ", "")
-            volumes = re.sub("(\d+)(\[a-zA-Z]+)", r"\1 \2", volumes)
-    
-        
+        volumes = ""
+        pattern = "\| \d+,?\d?\s?[MmLlCc][TtLlCc\s]"
+        results = re.findall(self.regex_patterns['volume'], string)
+        string = re.sub(self.regex_patterns['volume'], "", string)
+        for i, y in enumerate(results):
+            results[i] = results[i].strip().strip("|").strip()
+        volumes = " / ".join(results)
+           
+     
+        # #possible dimensions
+        # dimensions = ""
+        # pattern = "\S+\s[EeΕε][ΚκKk]\.?\s|[ΦΔΥ]?\d\S*[ΧχXx]?\S*\s?[ΕΚεκCMcm]{0,2}\.?"
+        # search_whole_string = re.findall(pattern, string)                                 #if the pattern repeats, then we can't know 
+        # search = re.findall(pattern, string.split("|")[-1])                               #what product the dimension references
+        # if len(search) == 1 and len(search_whole_string) == 1:                          #It's safe only if there is only a single match.
+        #     dimensions = search[0]
+        #     string = string.replace(dimensions, "").strip().strip("|").strip()
+        #     dimensions = dimensions.lower().replace("χ", "x")
 
-        
+
+        dimensions: str = ""
+        while re.search("(\d)[xΧχ](\d)", string):
+            string = re.sub("(\d)[xΧχ](\d)", r"\1X\2", string)
+        dimensions_pattern = "\| [ΦΔDF]?\s?\d[0-9X\-,\.]+\s?[EeΕεCc^M^m^Μ^μ^T^t^Τ^τ]?[ΚκKkMm^C^c]?\.?\s?"
+        dimension_results = re.findall(self.regex_patterns['dimension'], string)
+        if len(dimension_results) == 1:
+            dimensions = dimension_results[0].strip().strip("|").strip()
+            string = re.sub(f"\|\s{dimensions}", "", string)
+            string = re.sub("\s{2,}", "", string)       
 
 
-        #split title
-        breakpoints = re.findall("(.+?)\|", string + "|")
-        for i, y in enumerate(breakpoints):
-            breakpoints[i] = y.strip()
-        main_title = string
         rest = ""
-        if breakpoints:
-            if breakpoints[0]:
-                main_title = breakpoints.pop(0)
-                rest = " | ".join(breakpoints)
-
-        
-        #possible dimensions
-        dimensions = ""
-        pattern = "\S+\s[EeΕε][ΚκKk]\.?\s|[ΦΔΥ]?\d\S*[ΧχXx]?\S*\s?[ΕΚεκCMcm]{0,2}\.?"
-        search_whole_string = re.findall(pattern, rest)                                 #if the pattern repeats, then we can't know 
-        search = re.findall(pattern, rest.split("|")[-1])                               #what product the dimension references
-        if len(search) == 1 and len(search_whole_string) == 1:                          #It's safe only if there is only a single match.
-            dimensions = search[0]
-            rest = rest.replace(dimensions, "")
-            dimensions = dimensions.lower().replace("χ", "x")
-
-        # head = head.replace("ΜΕ ", "| ΜΕ ").replace("ΣΕ ", "| ΣΕ ")                   
-        # rest = rest.replace("ΜΕ ", "| ΜΕ ").replace("ΣΕ ", "| ΣΕ ")
-
-        main_title = main_title.replace("ΜΕ ", "| ΜΕ ").replace("ΣΕ ", "| ΣΕ ").replace("+ ", "| + ")   #primes the remaining title for
-        rest = rest.replace("ΜΕ ", "| ΜΕ ").replace("ΣΕ ", "| ΣΕ ").replace("+ ", "| + ")               #the next extraction function
                                                                                                         
 
         return {'og_title': og_title, 
-                'Title':main_title.title().strip(),
+                'Title':string.title().strip(),
                 'Rest':rest.title().strip(),
                 'Brand':brand.title().strip(), 
                 'Grouping':grouping.title().strip(), 
                 'SKU':code.upper().strip(),
-                'Dimensions':dimensions.lower().strip(),
+                'Dimensions':dimensions, #.lower().strip(),
                 'Series':series.title().strip(), 
                 'Misc':parenth.title().strip(), 
                 'Volume':volumes.title().strip(),
@@ -291,8 +288,11 @@ def main():
     titles_count = len(titles)
     data = []
     print("Analyzing titles:")
+    count = -1
     for i, title in enumerate(titles):
-        print(f"[{i+1} / {titles_count}]", end='\r')
+        # if i % int(len(titles)/10) == 0:
+        #     count += 1
+        # print(f"[{'|'*count}{' '*(10-count)}]", end='\r')
         data.append(ProductTitle(title).get_data())
     print(f"[{titles_count} / {titles_count}]")
     print("Analysis complete.")
@@ -304,4 +304,6 @@ def main():
 
 
 if __name__ == "__main__":
+    start = perf_counter()
     main()
+    print(perf_counter()-start)
